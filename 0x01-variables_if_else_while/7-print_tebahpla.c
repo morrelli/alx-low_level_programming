360cec45447634d66a62f7dde83751d116604c00
morrelli <111117172+morrelli@users.noreply.github.com>
2022-09-09 16:09:37 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,22 @@
-uu
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+char letter; /*Decarling statement*/
+
+/*for z-a*/
+for (letter = 122 ; letter >= 97; letter--)
+{
+putchar(letter);
+}
+
+putchar('\n'); /*new line*/
+
+return (0);
+
+}
