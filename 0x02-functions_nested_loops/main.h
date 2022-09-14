543c6fc6fefcc48820f1193ef33dbe30feef20c5
morrelli <111117172+morrelli@users.noreply.github.com>
2022-09-14 11:51:55 -0500

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,11 +8,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-<<<<<<< HEAD
-int print_last_digit(int);
-=======
 int print_last_digit(int nld);
->>>>>>> 87522f9f8c758435923abcef9016ae311e8789b9
 void jack_bauer(void);
 void times_table(void);
 int add(int one, int two);
