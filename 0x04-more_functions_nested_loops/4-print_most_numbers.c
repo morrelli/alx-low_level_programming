84b8b067b929d1ffd660435433cfee2c02771072
morrelli <111117172+morrelli@users.noreply.github.com>
2022-09-15 17:04:35 -0500

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,19 @@
 #include <stdio.h>
 
 /**
- * print_numbers - return 0-9
+ * print_most_numbers - return 0-9 skips 2 and 4
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		_putchar(i);
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
 	}
 	_putchar('\n');
 }
