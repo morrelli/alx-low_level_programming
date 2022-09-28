dfc0a78f9549dc55680f8b85f52375009b06b7ef
morrelli <111117172+morrelli@users.noreply.github.com>
2022-09-28 13:00:39 -0700

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
- *factorial - factorial of a given number
- *@n: pointer block of memory to fill
- *Return: factorial
+ *_pow_recursion - returns the value of x raised to the power of y
+ *@x: integer
+ *@y: integer
+ *Return: pow recursion
  */
 
-int factorial(int n)
+int _pow_recursion(int x, int y)
 {
-if (n == 0) /*Base condition*/
-
-return (1);
-
-else if (n < 0) /*Base condition*/
+if (y < 0) /*Base condition*/
+{
 return (-1);
-else
-return (n * factorial(n - 1)); /*Recursive call*/
+}
 
+else if (y != 0)
+return (x * _pow_recursion(x, y - 1)); /* Recursive call*/
+
+else
+return (1);
 }
